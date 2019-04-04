7d635388b59e7b2c84d51066a4324b673a9a2b91
Rajgourav Jain <rajgourav.jain@gmail.com>
2019-04-05 01:02:59 +0530

    Create Utilities.groovy

---
+++ in/100m/devops/jenkins/Utilities.groovy
@@ -0,0 +1,10 @@
+package in.100m.devops.jenkins
+
+
+class Utilities implements Serializable {
+  def steps
+  Utilities(steps) {this.steps = steps}
+  def mvn(args) {
+    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  }
+}
