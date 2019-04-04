ec48be0bd44715f59d63f67b0c0fc13d6be4f51c
Rajgourav Jain <rajgourav.jain@gmail.com>
2019-04-05 02:13:51 +0530

    maven tool name updated to maven_3.6.0

--- src/com/example/devops/jenkins/Utilities.groovy
+++ src/com/example/devops/jenkins/Utilities.groovy
@@ -5,6 +5,6 @@ class Utilities implements Serializable {
   def steps
   Utilities(steps) {this.steps = steps}
   def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+    steps.sh "${steps.tool 'maven_3.6.0'}/bin/mvn -o ${args}"
   }
 }
