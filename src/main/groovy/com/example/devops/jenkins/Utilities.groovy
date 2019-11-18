9ae7d0281ffeace8cc5a2fbb4dab10215e35bcd7
rajgouravj <rajgouravj@zendrive.com>
2019-11-19 01:28:48 +0530

    Adding unit test for jenkins pipeline job

---
+++ src/main/groovy/com/example/devops/jenkins/Utilities.groovy
@@ -0,0 +1,9 @@
+package com.example.devops.jenkins
+
+class Utilities implements Serializable {
+    def steps
+    Utilities(steps) {this.steps = steps}
+    def mvn(args) {
+        steps.sh "${steps.tool 'maven_3.6.2'}/bin/mvn -Dmaven.test.skip=true ${args}"
+    }
+}
