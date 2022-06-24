a47e5911fc0264235d6bf148968612c845e429ef
Rajgourav Jain <Rajgourav.Jain@gmail.com>
2022-06-24 20:05:20 +0530

    feat(config): read jenkins config file

--- src/com/example/devops/jenkins/Utilities.groovy
+++ src/com/example/devops/jenkins/Utilities.groovy
@@ -1,9 +1,18 @@
 package com.example.devops.jenkins
-
+@Grab('org.yaml:snakeyaml:1.17')
+    
+import org.yaml.snakeyaml.Yaml
+    
 class Utilities implements Serializable {
     def steps
     Utilities(steps) {this.steps = steps}
     def hello(args) {
         steps.sh "echo Hello Jenkins ${args}"
     }
+    def get_config(jenkins_config_yaml){
+      println jenkins_config_yaml
+      Yaml parser = new Yaml()
+      def jconf = parser.load((jenkins_config_yaml as File).text)
+      return jconf 
+
 }
